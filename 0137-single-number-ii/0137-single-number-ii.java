3e6bc41ccdaf7958e779a7d4376fbf6494926d63
Anshika Agarwal <91412230+anshika9946@users.noreply.github.com>
2023-07-21 00:10:07 +0530

    Time: 2 ms (78.69%), Space: 44.3 MB (38.05%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -1,17 +1,15 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
-        
-        for (int x : nums) {
-            map.put(x, map.getOrDefault(x, 0) + 1);
-        }
+ int ans = 0;
 
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            if (entry.getValue() == 1) {
-                return entry.getKey();
-            }
-        }
-        
-        return -1;
+    for (int i = 0; i < 32; ++i) {
+      int sum = 0;
+      for (final int num : nums)
+        sum += num >> i & 1;
+      sum %= 3;
+      ans |= sum << i;
+    }
+
+    return ans;
     }
 }
